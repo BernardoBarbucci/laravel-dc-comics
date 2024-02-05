0b66fa5688dc882e4227ac61f3114d196798e2d3
BernardoBarbucci <bernardobarbucci@gmail.com>
2024-02-05 19:16:22 +0100

    eseguito rollback e cambiata la tabella in base ai tipi di dati che ci sono nel bonus per renderla coerente e adatta

--- database/migrations/2024_02_05_174617_create_fumettos_table.php
+++ database/migrations/2024_02_05_174617_create_fumettos_table.php
@@ -14,12 +14,14 @@ return new class extends Migration
         Schema::create('fumettos', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('author');
             $table->text('description');
-            $table->integer('publication_year');
+            $table->string('thumb');
             $table->decimal('price', 8, 2);
-            $table->string('genre');
-            $table->boolean('available')->default(true);
+            $table->string('series');
+            $table->date('sale_date');
+            $table->string('type');
+            $table->json('artists');
+            $table->json('writers');
             $table->timestamps();
         });
     }
