34f214bbd11574269d1ff507b3d14956a1b8c768
BernardoBarbucci <bernardobarbucci@gmail.com>
2024-02-06 22:30:01 +0100

    aggiornata la show

--- resources/views/fumetti/show.blade.php
+++ resources/views/fumetti/show.blade.php
@@ -1,17 +1,41 @@
 @extends('layout.app')
 
 @section('content')
+    {{-- <p><strong>Artists:</strong> {{ implode(', ', $fumetto->artists) }}</p>
+    <p><strong>Writers:</strong> {{ implode(', ', $fumetto->writers) }}</p> --}}
 
-    <h1>{{ $fumetto->title }}</h1>
+    <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">{{ $fumetto->title }}</h5>
+            <p class="card-text">
+                <strong>Description:</strong> {{ $fumetto->description }}
+            </p>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">
+            <p><strong>Price:</strong> {{ $fumetto->price }}</p>
+          </li>
+          <li class="list-group-item">
+            <p><strong>Series:</strong> {{ $fumetto->series }}</p>
+          </li>
+          <li class="list-group-item">
+            <p><strong>Sale Date:</strong> {{ $fumetto->sale_date }}</p>
+          </li>
+          <li class="list-group-item">
+            <p><strong>Type:</strong> {{ $fumetto->type }}</p>
+          </li>
+          {{-- <li class="list-group-item">
+            <p>QUA CI ANDREBBERO GLI ARTISTS</p>
+          </li>
 
-    <p><strong>Description:</strong> {{ $fumetto->description }}</p>
-    <p><strong>Price:</strong> {{ $fumetto->price }}</p>
-    <p><strong>Series:</strong> {{ $fumetto->series }}</p>
-    <p><strong>Sale Date:</strong> {{ $fumetto->sale_date }}</p>
-    <p><strong>Type:</strong> {{ $fumetto->type }}</p>
-    <p><strong>Artists:</strong> {{ implode(', ', $fumetto->artists) }}</p>
-    <p><strong>Writers:</strong> {{ implode(', ', $fumetto->writers) }}</p>
-    
-    <a href="{{ route('fumetti.index') }}">Torna alla lista dei fumetti</a>
-
+          <li class="list-group-item">
+            <p>QUA CI ANDREBBERO GLI WRITERS</p>
+          </li> --}}
+        </ul>
+        <div class="card-body">
+          {{-- <a href="#" class="card-link">Card link</a> --}}
+          <a href="{{ route('fumetti.index') }}">Torna alla lista dei fumetti</a>
+        </div>
+      </div>
 @endsection
